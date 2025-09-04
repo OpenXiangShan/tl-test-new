4563e1d760de22bb246afd41a4998ca53a57cb7a
XiChen <chenxi171@mails.ucas.ac.cn>
2025-09-04 18:02:49 +0800

    misc: fix trace path bug

--- main/System/TLSystem.cpp
+++ main/System/TLSystem.cpp
@@ -88,7 +88,7 @@ void TLInitialize(TLSequencer** tltest, PluginManager** plugins, std::function<v
     tlcfg.fuzzStreamStart               = CFUZZER_FUZZ_STREAM_START;
     tlcfg.fuzzStreamEnd                 = CFUZZER_FUZZ_STREAM_END;
 
-    tlcfg.traceFilePath                 = NULL; // as default
+    tlcfg.traceFilePath                 = ""; // as default
 
     tlcfgInit(tlcfg);
 
